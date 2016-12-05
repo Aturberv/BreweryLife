7fb2fb6fa4fd2ec73c5f6100e7055c437fc913f8
Aleksander Turberville <aleksville@live.com>
2016-12-05 13:18:07 -0500

    inlines brewImages into the return statement

--- src/BreweryImages.js
+++ src/BreweryImages.js
@@ -2,17 +2,15 @@ import React from 'react';
 import { Carousel } from 'react-bootstrap';
 
 const BreweryImage = ({brewery}) => {
-	const brewImages = brewery.photos.map((photo, idx) => {
-		return (
-			<Carousel.Item key={idx}>
-				<img key={idx} width={600} height={250} src={photo} role="presentation" />
-			</Carousel.Item>
-		);
-	});
-
 	return (
 		<Carousel>
-			{brewImages}
+			{
+				brewery.photos.map((photo, idx) =>
+					<Carousel.Item key={idx}>
+						<img key={idx} width={600} height={250} src={photo} role="presentation" />
+					</Carousel.Item>
+				)
+			}
 		</Carousel>
 	);
 }
