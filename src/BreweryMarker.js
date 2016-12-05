51a0ac3a6c1ec16cfbb07d5924b1eccc1c9222e9
Aleksander Turberville <aleksville@live.com>
2016-12-04 20:07:51 -0500

    style the modal a bit, add some ugly hovers to display info

--- src/BreweryMarker.js
+++ src/BreweryMarker.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Popover, ButtonToolbar, OverlayTrigger } from 'react-bootstrap';
 import './BrewModal.css';
 
 class BreweryMarker extends Component {
 	state = {
 		isShowingModal: false,
 	}
+
   render() {
   	let close = () => this.setState({ isShowingModal: false });
 
+ 	const popoverHoverFocus = (
+		<Popover id="popover-trigger-hover-focus" title={this.props.brewery.name}>
+			<ul>
+				<li>{this.props.brewery.rating} stars</li>
+				<li>{this.props.brewery.reviews} reviews</li>
+			</ul>
+		</Popover>
+	);
+
     return (
       <div className="modal-container" style={{height: 200}}>
+      <ButtonToolbar>
+        <OverlayTrigger trigger={['hover', 'focus']} placement="bottom" overlay={popoverHoverFocus}>
       	<div onClick={() => this.setState({ isShowingModal: true })} >
       		<img 
       			src={require('../public/beer_pin-3.png')} 
       			role="presentation" 
       		/>
       	</div>
+      	</OverlayTrigger>
+      </ButtonToolbar>
 
         <Modal
         	show={this.state.isShowingModal}
