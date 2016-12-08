98f5587b854cea69cc6aec38a2a03b7b558b678e
Aleksander Turberville <aleksville@live.com>
2016-12-07 19:56:55 -0500

    add beers to the modal, needs to be styled

--- src/BreweryBeers.js
+++ src/BreweryBeers.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 const BreweryBeers = ({brewery}) => {
 	return (
+		<div>
 		{
-			brewery.beers.map((beer) =>
-				<div className="brewery-beers">
-    				<img src={beer.beerLabel} />
-    				<h3>beer.beerName</h3>
-    				<h4>beer.beerStyle</h4>
-    				<p>beer.beerDescription</p>
-    				<span>rating: beer.beerRating, reviews: beer.beerRatingCount</span>
+			brewery.beers.map((beer, idx) =>
+				<div key={beer, idx} className="brewery-beers">
+				<span>
+    				<img src={beer.beerLabel} role="presentation" />
+    				<h3>{beer.beerName}</h3>
+    			</span>
+    				<h4>{beer.beerStyle}</h4>
+    				<p>{beer.beerDescription}</p>
+    				<p>rating: {beer.beerRating} reviews: {beer.beerRatingCount}</p>
     			</div>
+    		)
     	}
-	)
+    	</div>
+	);
 }
 
 export default BreweryBeers;
