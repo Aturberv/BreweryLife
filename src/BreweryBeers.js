3b01b43cd634d299d46a2be165289ac5abf7c13d
Aleksander Turberville <aleksville@live.com>
2016-12-08 14:34:19 -0500

    style the modal up a bit

--- src/BreweryBeers.js
+++ src/BreweryBeers.js
@@ -6,13 +6,15 @@ const BreweryBeers = ({brewery}) => {
 		{
 			brewery.beers.map((beer, idx) =>
 				<div key={beer.beerName} className="brewery-beers">
-    					<img className="beer-label" src={beer.beerLabel} role="presentation" />
-    				<div className="beer-info">
+    				<img className="beer-label" src={beer.beerLabel} role="presentation" />
+                    <div className="beer-info">
     					<h3>{beer.beerName}</h3>
     					<h4>{beer.beerStyle}</h4>
     					<p>{beer.beerDescription}</p>
+                    <div className="beer-stats">
     					ABV and IBU go here.
     					<p>rating: {beer.beerRating} reviews: {beer.beerRatingCount}</p>
+                    </div> 
     				</div>
     			</div>
     		)
