5d515cf8e044c52dcbf1e3ce3793d17c404e4ffc
Aleksander Turberville <aleksville@live.com>
2016-12-09 16:47:32 -0500

    implement social links to json and social icons to brewerypage

--- update-breweries.js
+++ update-breweries.js
@@ -130,7 +130,14 @@ function parseUntappdResponse(response) {
         untappdRating: brewery.rating.rating_score,
         breweryDescription: brewery.brewery_description,
         breweryLogo: brewery.brewery_label,
-        social: brewery.contact, 
+        social: brewery.contact.map(socialUrl){
+            return {
+                twitter: `https://twitter.com/${socialUrl.twitter}`,
+                instagram: `href: 'https://www.instagram.com/${socialUrl.instagram}`,
+                facebook: socialUrl.facebook,
+                website: socialUrl.url
+            }
+        },
         beers: breweryBeers.map(function(beerObj){
             return {
                 beerName: beerObj.beer.beer_name,
