71adb2fb317efc3eb29cd2a1f866de69d3d2f793
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2016-12-09 09:50:29 -0500

    Remove modal and associated complexity

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import GoogleMap from 'google-map-react';
 import autoBind from 'react-autobind';
 import BreweryMarker from './BreweryMarker';
 import BreweryFilter from './BreweryFilter';
-import breweries from './breweries.json';
+import BreweryPage from './BreweryPage';
+import Breweries from './breweries.json';
+import ReactGA from 'react-ga';
+
 import './App.css';
 
-const Breweries = breweries.Breweries;
 const API_KEY = 'AIzaSyCG6SNlthILXRA7qZhcvNH5Wx6NL42gE8Y';
 
 class App extends Component {
@@ -16,59 +18,85 @@ class App extends Component {
     autoBind(this)
     this.state = {
       breweries: Breweries,
-      breweryShowing: false
+      brewery: null,
+      defaultCenter: {lat: 40.7132859, lng: -73.9285485}
     }
   }
 
   filterBreweries(type, value) {
     
     this.setState({
-      breweries: Breweries.filter((brewery) => {
+      breweries: Object.entries(Breweries).reduce((result, [breweryName, brewery]) => {
         switch(typeof (value)){
           case 'number':
-            return brewery[type] >= value;
+            if(brewery[type] >= value){
+              result[breweryName] = brewery
+            }
+            break;
           case 'string':
-            return brewery[type].toLowerCase().includes(value.toLowerCase())
+            if(brewery[type].toLowerCase().includes(value.toLowerCase())){
+              result[breweryName] = brewery
+            }
+            break;
           default:
-            return true;
+            return result;
         }
-        
-      })
+        return result;
+      }, {})
     });
   }
 
-  breweryShowingToggle() {
+  trackBreweryView(breweryName) {
+    breweryName ?
+        ReactGA.modalview(breweryName)
+      : ReactGA.pageview('/') && ReactGA.event({
+          category: 'Modal',
+          action: 'Closed',
+          label: breweryName
+        });
+  }
+
+  selectBrewery(breweryName) {
     this.setState({
-      breweryShowing: !this.state.breweryShowing
+      brewery: this.state.breweries[breweryName]
     });
+    this.trackBreweryView(breweryName);
   }
 
   render() {
     return (
       <div className="App">
         <BreweryFilter onFilter={ this.filterBreweries }
-                       breweryShowing={ this.state.breweryShowing }
-                       breweryShowingToggle={ this.breweryShowingToggle } /> 
+                       brewery={ this.state.brewery }
+                       closeBrewery={ () => this.selectBrewery(null) } /> 
+        <div>
+        {
+          this.state.brewery &&
+            <div className="brewery-page">
+              <BreweryPage closeBrewery={ () => this.selectBrewery(null) }
+                           brewery={this.state.brewery}
+              />
+            </div>
+        }
         <div className="App-map">
           <GoogleMap
             bootstrapURLKeys={{ key: API_KEY, language: 'en' }}
-              defaultCenter={ {lat: 40.7132859, lng: -73.9285485} }
-              defaultZoom={ 12 }
-
+            defaultCenter={ this.state.defaultCenter }
+            defaultZoom={ 12 }
+            onChildClick={ this.selectBrewery }
           >
           {
-            this.state.breweries.map((brewery) => 
+            Object.entries(this.state.breweries).map(([breweryName, brewery]) => 
               <BreweryMarker 
-                             key={ brewery.name }
-                             lat={ brewery.location.lat }
-                             lng={ brewery.location.lng }
-                             brewery={ brewery }
-                             breweryShowingToggle={ this.breweryShowingToggle }
-                             breweryShowing={ this.state.breweryShowing }
+                 key={ breweryName }
+                 lat={ brewery.location.lat }
+                 lng={ brewery.location.lng }
+                 brewery={ brewery }
               />
             )
           }
-        </GoogleMap>
+          </GoogleMap>
+        </div>
         </div>
       </div>
     );
--- src/BreweryMarker.js
+++ src/BreweryMarker.js
@@ -1,37 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { OverlayTrigger, Popover } from 'react-bootstrap';
 import './BrewModal.css';
-import ReactGA from 'react-ga';
-import BreweryPage from './BreweryPage';
-import autoBind from 'react-autobind';
 
-class BreweryMarker extends Component {
-  constructor(props) {
-    super(props);
-    autoBind(this);
-    this.state = {
-      isShowing: false,
-    }
-  }
-
-  toggle() {
-    !this.state.isShowing ?
-        ReactGA.modalview(this.props.brewery.name)
-      : ReactGA.pageview('/') && ReactGA.event({
-          category: 'Modal',
-          action: 'Closed',
-          label: this.props.brewery.name});
-    this.setState({ isShowing: !this.state.isShowing });
-    this.props.breweryShowingToggle();
-  }
-
-  render() {
-    const {
-      brewery
-    } = this.props;
-
-    const popoverHoverFocus = (
-      <Popover id="popover-trigger-hover-focus" title={this.props.brewery.name}>
+const BreweryMarker = ({
+  brewery
+}) => {
+  const popoverHoverFocus = (
+      <Popover id="popover-trigger-hover-focus" title={brewery.name}>
         <div>
           <div>{`Yelp: ${brewery.yelpRating}/5 (${brewery.yelpNumReviews} reviews)`}</div>
           <div>{`Google: ${brewery.googleRating}/5`}</div>
@@ -44,20 +19,11 @@ class BreweryMarker extends Component {
         <OverlayTrigger trigger={['hover', 'focus']} 
                         placement="top" 
                         overlay={popoverHoverFocus}>
-      	<div onClick={this.toggle} >
-      	 <img src={require('./images/beer_pin-3.png')} 
+         <img src={require('./images/beer_pin-3.png')} 
               role="presentation" />
-        </div>
         </OverlayTrigger>
-        <div className="brewery-page">
-        <BreweryPage isShowing={this.state.isShowing && this.props.breweryShowing}
-                     toggle={this.toggle}
-                     brewery={brewery}
-        />
-        </div>
       </div>
    );
-  }
 };
 
 export default BreweryMarker;
