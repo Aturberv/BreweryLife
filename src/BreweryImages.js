ab1cc5671c908ae32ef46b15790648f3058d5d43
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2016-12-10 11:54:02 -0500

    Fix carousel arrows

--- src/BreweryImages.js
+++ src/BreweryImages.js
@@ -1,27 +1,26 @@
 import React from 'react';
-//import { Carousel } from 'react-bootstrap';
 import Carousel from 'antd/lib/carousel';
 import 'antd/lib/carousel/style/css'
 import Icon from 'antd/lib/icon';
 import 'antd/lib/icon/style/css';
-import './BrewImage.css';
+import './BrewImages.css';
 
 const BreweryImage = ({brewery}) => {
 	return (
 		<Carousel arrows={true}
 				  infinite={true}
 				  accessibility={true}
-				  nextArrow={<span><Icon type="right"/>omg</span>}
-				  prevArrow={<span><Icon type="left"/>}omg</span>}	
+				  nextArrow={<Icon type="right"/>}
+				  prevArrow={<Icon type="left"/>}	
 				  dots={false}
 				  lazyLoad={true}
-				  swipe={true}
+				  draggable={false} //disable dragging on desktop
 				  swipeToSlide={true}
 				  >
 			{
 				brewery.photos.map((photo, idx) =>
 					<div key={idx}>
-						<img className="brew-images" key={idx} src={photo} role="presentation" />
+						<img className="brew-image" key={idx} src={photo} role="presentation" />
 					</div>
 				)
 			}
