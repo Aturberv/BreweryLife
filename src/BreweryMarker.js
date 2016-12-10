cf3d4060b76b944090092af9eb249a4f1689dbdb
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2016-12-09 23:59:52 -0500

    Use react-router to enable deep linking

--- src/BreweryMarker.js
+++ src/BreweryMarker.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { OverlayTrigger, Popover } from 'react-bootstrap';
+import { Link } from 'react-router';
 import './BreweryMarker.css';
 
 const BreweryMarker = ({
-  brewery
+  brewery,
+  breweryKey
 }) => {
   const popoverHoverFocus = (
       <Popover id="popover-trigger-hover-focus" title={brewery.name}>
@@ -15,14 +17,16 @@ const BreweryMarker = ({
     );
 
     return (
-      <div className="brewery-pin">
-        <OverlayTrigger trigger={['hover', 'focus']} 
-                        placement="top" 
-                        overlay={popoverHoverFocus}>
-         <img src={require('./images/beer_pin-3.png')} 
-              role="presentation" />
-        </OverlayTrigger>
-      </div>
+      <Link to={`/${breweryKey}`}>
+        <div className="brewery-pin">
+          <OverlayTrigger trigger={['hover', 'focus']} 
+                          placement="top" 
+                          overlay={popoverHoverFocus}>
+           <img src={require('./images/beer_pin-3.png')} 
+                role="presentation" />
+          </OverlayTrigger>
+        </div>
+      </Link>
    );
 };
 
