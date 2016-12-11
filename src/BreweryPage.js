9c2758d81fba534833db9825a95935d7e00446c8
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2016-12-10 20:57:13 -0500

    Add Uber button

--- src/BreweryPage.js
+++ src/BreweryPage.js
@@ -5,21 +5,20 @@ import BreweryDescription from './BreweryDescription';
 import BreweryEvents from './BreweryEvents';
 import BreweryBeers from './BreweryBeers';
 import BrewerySocial from './BrewerySocial';
-
+import RideButton from './RideButton';
 import Tabs from 'antd/lib/tabs';
 import 'antd/lib/tabs/style/css';
 
 import './BreweryPage.css';
 
-const BreweryPage = ({
-    brewery
-}) => {
+const BreweryPage = ({brewery}) => {
     return (
       <div className="breweryPage">
             <div className="breweryPage-header">
                 <h2 className="brewery-name">{brewery.name}</h2>
                 <center><BrewerySocial social={brewery.social} /></center>
             </div>
+            <RideButton breweryName={brewery.name} destination={brewery.location}/>
             <div className="breweryPage-tabs">
                 <Tabs defaultActiveKey="1">
                     <Tabs.TabPane key="1" tab="About">
