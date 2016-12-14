68f4c2ecffdb40b3160e3c2586f055b7502bbe35
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2016-12-13 23:29:23 -0500

    Add meta keywords

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,22 +1,13 @@
 var breweries = require('./breweryIds.json');
 var xml = require('xml');
 var fs = require('fs');
-var async = require('async')
 
+var siteMap = [{ urlset: Object.keys(breweries).map(function(key){
+    return {
+        url: [{
+            loc: `https://nycbrewerymap.com/${key}`
+        }]
+    }
+}) }];
 
-var siteMap = [{ urlset: [{}] }];
-
-var siteMapObj = Object.keys(breweries).map(function(key){
-	return {
-		url: [{
-			loc: `https://nycbrewerymap.com/${key}`
-		}]
-	}
-})
-
-var siteMap = [{ urlset: siteMapObj }]
-
-setTimeout(function writeSiteMap() {
-	console.log(siteMap)
-	fs.writeFile('./public/sitemap.xml', xml(siteMap, { declaration: true }))
-}, 3000);
+fs.writeFile('./public/sitemap.xml', xml(siteMap, { declaration: true }))
