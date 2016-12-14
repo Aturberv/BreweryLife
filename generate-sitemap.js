fb5f23664d48db45f1fb15e87a9dfba38a6c32bb
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2016-12-14 00:20:11 -0500

    Update sitemap with more metadata

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,13 +1,27 @@
 var breweries = require('./breweryIds.json');
 var xml = require('xml');
 var fs = require('fs');
+var async = require('async')
 
-var siteMap = [{ urlset: Object.keys(breweries).map(function(key){
-    return {
-        url: [{
-            loc: `https://nycbrewerymap.com/${key}`
-        }]
-    }
-}) }];
+var homeLink = {
+    url: [
+        {loc:"https://nycbrewerymap.com/"},
+        {lastmod: new Date().toISOString()},
+        {changefreq: "daily"},
+        {priority:1.0}
+    ]
+}
+var brewLinks = Object.keys(breweries).map(function(key){
+                            return {
+                                url: [
+                                    {loc: `https://nycbrewerymap.com/${key}`},
+                                    {lastmod: new Date().toISOString()},
+                                    {changefreq: "daily"}
+                                ]
+                            }
+                        });
+
+var siteMap = [{ urlset: [{_attr:{xmlns:"http://www.sitemaps.org/schemas/sitemap/0.9"}}].concat(homeLink).concat(brewLinks) }]
+
 
 fs.writeFile('./public/sitemap.xml', xml(siteMap, { declaration: true }))
