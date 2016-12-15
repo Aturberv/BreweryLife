dd335bde53fb941fde0bc92627f1317b875d7e3a
Aleksander Turberville <aleksville@live.com>
2016-12-14 21:49:57 -0500

    WIP of image reworking

--- src/BreweryImages.js
+++ src/BreweryImages.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import { Row, Col } from 'react-bootstrap';
-import ImageGallery from 'react-image-gallery';
+import React, { Component } from 'react';
+import { ReactRpg } from 'react-rpg';
 import "react-image-gallery/styles/css/image-gallery.css";
 import './BrewImages.css';
 
-const BreweryImage = ({brewery}) => {
-	const images = brewery.photos.map((photo) => {return {original: photo}});
+const previewStyles = {
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  position: 'fixed',
+  border: 'solid #1a1a1a 10px',
+  zIndex: '3',
+};
 
-	return (
-		<Row>
-		<Col lg={8} lgOffset={2}
-			 md={10} mdOffset={1} 
-			 sm={12}>
-			<ImageGallery items={images}
-						  lazyLoad={true}
-						  showThumbnails={false}
-						  showPlayButton={false}/>
-		</Col>
-	    </Row>
-	);
+const ImagePreview = ({ url }) => <img src={url} style={previewStyles} alt={url} width="50%" height="auto" />;
+
+class BreweryImage extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			popUp: false,
+		};
+	}
+
+	getImages() {
+		const brewery = this.props.brewery
+		const images = brewery.photos.map((photo) => {
+			return {
+				url: photo,
+				clickHandler: (url, obj) => {
+					this.setState({ popUp: url });
+					console.log(obj);
+					setTimeout(() => this.setState({ popUp: false}), 1000)
+				}
+			}
+		})
+		return images;
+	}
+
+	render() {
+		return (
+			<div>
+
+			{ this.state.popUp !== false ? <ImagePreview url={this.state.popUp} /> : null }
+
+				<ReactRpg 
+					imagesArray={this.getImages()} 
+					columns={[1, 2, 5]} 
+					padding={10} 
+				/>
+			</div>
+		)
+	}
+
 }
 
 export default BreweryImage;
