dea13e10303ee4c1310b32bdbb1ca258db510281
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2016-12-14 21:44:50 -0500

    Gzip plaintext assets

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@ set -eux
 npm run generate-sitemap
 npm run update-breweries
 npm run build
+# gzip swag
+find build \( -iname '*.html' -o  -iname '*.css' -o -iname '*.js' -o -iname '*.json' -o -iname '*.xml' -o -iname '*.txt' -o -iname '*.ico' \-o -iname '*.eot' -o -iname '*.ttf' -o -iname '*.woff*' \)  -exec gzip -9 -n {} \; -exec mv {}.gz {} \;
 pip install --user awscli
-aws s3 sync build s3://nycbrewerymap/ --delete
+aws s3 sync build s3://nycbrewerymap/ --content-encoding 'gzip' --delete
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id E3N8Q7PC3NRSYR --paths "/*"
