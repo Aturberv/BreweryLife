68202aecc9ff750b5439dd109a0d5fcac819bf54
Aleksander Turberville <aleksville@live.com>
2016-12-15 16:17:06 -0500

    a better carousel

--- src/BreweryImages.js
+++ src/BreweryImages.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
-import Gallery from 'react-photo-gallery';
+import React from 'react';
 import "react-image-gallery/styles/css/image-gallery.css";
 import './BrewImages.css';
 
 const BreweryImage = ({brewery}) => {
-
-	const images = brewery.photos.map((photo) => {
-		return {
-			src: photo,
-			lightboxImage:{
-				src: photo
-			}
-		}
-	});
-
 	return(
-		<Gallery photos={images} />
+		<div className="row">
+			<div className="row__inner">
+			{
+				brewery.photos.map((photo) =>
+					<div className="tile">
+						<div className="tile__media">
+							<img className="tile__image"
+								src={photo} />
+						</div>
+					</div>
+				)
+			}
+			</div>
+		</div>
 	)
-
 }
 
 
