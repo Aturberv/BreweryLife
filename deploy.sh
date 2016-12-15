615565e53784beff36fb54b56df521b2d58bd2d9
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2016-12-14 22:32:50 -0500

    Add Cache-Control headers to S3 objects for 2 days

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ npm run build
 # gzip swag
 find build \( -iname '*.html' -o  -iname '*.css' -o -iname '*.js' -o -iname '*.json' -o -iname '*.xml' -o -iname '*.txt' -o -iname '*.ico' \-o -iname '*.eot' -o -iname '*.ttf' -o -iname '*.woff*' \)  -exec gzip -9 -n {} \; -exec mv {}.gz {} \;
 pip install --user awscli
-aws s3 sync build s3://nycbrewerymap/ --content-encoding 'gzip' --delete
+aws s3 sync build s3://nycbrewerymap/ --content-encoding 'gzip' --cache-control max-age=172800 --delete
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id E3N8Q7PC3NRSYR --paths "/*"
