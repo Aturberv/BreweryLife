c4ac0eabbe990de3215c7f32d782b71e746c482c
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2016-12-21 00:24:49 -0500

    Add Amazon ads
    Also, clean up some small errors

--- src/BreweryDescription.test.js
+++ src/BreweryDescription.test.js
@@ -3,20 +3,9 @@ import { shallow } from 'enzyme';
 import sinon from 'sinon';
 import BreweryDescription from './BreweryDescription';
 
-describe('BreweryDescription', function() {
-
-  it('contains description thats passed to it', () => {
-    const description = 'omfg lol';
-    const breweryDescription = shallow(<BreweryDescription description={description}/>);
-    expect(breweryDescription.contains(description)).toEqual(true);
-  });
-
-  it('requires a description', () => {
-    const stub = sinon.stub(console, 'error');
-    const breweryDescription = shallow(<BreweryDescription />);
-    expect(stub.calledOnce).toEqual(true);
-    expect(stub.getCall(0).args[0].indexOf('required')).toBeGreaterThan(-1)
-  });
-
+it('contains description thats passed to it', () => {
+  const description = 'omfg lol';
+  const breweryDescription = shallow(<BreweryDescription description={description}/>);
+  expect(breweryDescription.contains(description)).toEqual(true);
 });
 
