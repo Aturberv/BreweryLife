e9134f3cb1a2bf6248f20b2d1b0034538136a5d6
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2016-12-25 22:39:25 -0500

    Use chrome on travis

--- deploy/scraper.js
+++ deploy/scraper.js
@@ -1,19 +1,23 @@
-require('chromedriver');
 var breweries = require('./breweryIds.json');
 var fs = require('fs');
-var webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until;
+require('chromedriver');
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 
-var driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .build();
+const options = new chrome.Options();
+options.addArguments('no-sandbox');
+
+const builder = new webdriver.Builder();
+builder.forBrowser('chrome');
+builder.setChromeOptions(options);
+
+const driver = builder.build();
 
 var pageUrls = ['index.html'].concat(Object.keys(breweries));
 
 pageUrls.forEach(function(url) {
     driver.get('http://localhost:8080/' + url)
-    driver.findElement(By.tagName('html')).then(function(el) {
+    driver.findElement(webdriver.By.tagName('html')).then(function(el) {
         el.getAttribute('outerHTML').then(function(html){
             console.log('Generating: ' + url);
             fs.writeFileSync('ssr/' + url, html);
