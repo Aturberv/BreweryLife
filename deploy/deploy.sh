1c5235ebc2e1dc8a22e9cb78446196bbbb0da198
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2017-01-06 00:13:53 -0500

    Gzip nested server side rendered files

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,8 +6,7 @@ function cleanup {
 }
 trap cleanup EXIT
 
-rm -rf ssr
-mkdir -p ssr
+rm -rf ssr/
 
 npm run generate-sitemap
 npm run update-breweries
@@ -17,7 +16,9 @@ npm run scrape
 
 # gzip swag
 find build \( -iname '*.html' -o  -iname '*.css' -o -iname '*.js' -o -iname '*.json' -o -iname '*.xml' -o -iname '*.txt' -o -iname '*.ico' \-o -iname '*.eot' -o -iname '*.ttf' -o -iname '*.woff*' \)  -exec gzip -9 -n {} \; -exec mv {}.gz {} \;
-find ssr/* -exec gzip -9 -n {} \; -exec mv {}.gz {} \;
+gzip -9 -n ssr/index.html; mv ssr/index.html.gz ssr/index.html;
+find ssr/**/* -exec gzip -9 -n {} \; -exec mv {}.gz {} \;
+
 pip install --user awscli
 aws s3 sync build/ s3://nycbrewerymap/ --content-encoding 'gzip' --cache-control max-age=172800 --delete
 aws s3 sync ssr/ s3://nycbrewerymap/ --content-type 'text/html' --content-encoding 'gzip' --cache-control max-age=172800
