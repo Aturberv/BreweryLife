3e69061ff77c5d1b25d9b32374d782d0a7ce323f
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2017-02-01 22:15:50 -0500

    Remove external js dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import App from './App/App';
 import config from '../config.json'
 import BreweryPage from './BreweryPage/BreweryPage';
+import 'babel-polyfill';
+import Raven from 'raven-js';
 import { Router, Route, browserHistory, IndexRedirect, Redirect } from 'react-router';
 import './index.css';
+
+ReactGA.initialize('UA-88592303-1');
+Raven.config('https://c56bf372c3244e089766fb089d1fb85a@sentry.io/122382', 
+    {
+       whitelistUrls: [/https:\/\/nycbrewerymap\.com.*/]
+    }).install()
 
 ReactDOM.render( 
   <Router history={browserHistory}>
